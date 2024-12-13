c376ec212c59f8ac2df88fdbe96d7b684f4623ed
Matija Osrečki <github@matijao.com>
2024-12-13 14:13:51 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-6 max-w-md mx-auto min-h-screen" >
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="postcss">
+:root {
+  color-scheme: dark;
+}
+
+body {
+  @apply bg-black text-zinc-100
+}
+</style>
+
+<script setup>
+useHead({
+  title: 'nutlet',
+  description: 'cashu web wallet',
+  meta: [
+    {
+      name: 'color-scheme',
+      content: 'dark'
+    }
+  ]
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="italic text-xl text-orange-300">nutlet</h1>
+    <p class="italic">cashu web wallet</p>
+  </div>
+</template>
